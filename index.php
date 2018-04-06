15e8360cac897d48dd748e533f68f1ec48199a03
Pillon <pillonsteve@gmail.com>
2018-04-06 21:52:11 +0200

    Mise à jour de l'Api ajout de la première partie du dashboard

--- index.php
+++ index.php
@@ -1,13 +1,11 @@
 <?php
  
 require "vendor/autoload.php";
-
 function connect_to_db()
 {
 	$servername = "localhost";
 	$username = "root";
 	$password = "";
-
 	try {
 		    $conn = new PDO("mysql:host=$servername;dbname=events", $username, $password);
 		    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -21,24 +19,14 @@ function connect_to_db()
 
 $app = new Slim\App();
 
-$app->get('/api/events/{id}',function($request, $response, $args){
-$conn = connect_to_db();
-$tmp = $args['id'];
-$req = $conn->query("SELECT * FROM events WHERE cookie_id=$tmp;");
-foreach($req as $ligne)
-{
-	echo $ligne['name'].$ligne['cookie_id'].$ligne['referrer'].$ligne['createdAt'];
-}
-});
-
 $app->get("/api/events",function(){
 $conn = connect_to_db();
 $req = $conn->query("SELECT * FROM events");
+echo "Liste des événements: ";
 foreach($req as $ligne)
 {
-	echo $ligne['name'].$ligne['cookie_id'].$ligne['referrer'].$ligne['createdAt'];
+	echo "<br>".$ligne['name'];
 }
-echo "hello";
 });
 
 $app->post("/api/events",function($request){
@@ -47,6 +35,26 @@ $body = $request->getBody();
 $req = json_decode($body);
 $res = $conn->query("INSERT INTO events (name, referrer) VALUES ('$req->name', '$req->referrer');");
 });
+
+$app->get('/api/events/{id}',function($request, $response, $args){
+$conn = connect_to_db();
+$tmp = $args['id'];
+$req = $conn->query("SELECT * FROM events WHERE cookie_id = $tmp;");
+foreach($req as $ligne)
+{
+	echo "Nom de l'événement: ".$ligne['name']."<br>"." Référence de l'événement: ".$ligne['referrer']."<br>"." Date de création: ".$ligne['createdAt'];
+}
+});
+
+$app->get("/api/dashboard",function(){
+$conn = connect_to_db();
+$req = $conn->query("SELECT * FROM events");
+foreach($req as $ligne)
+{
+	echo "Nom de l'événement: ".$ligne['name']."<br>"." Référence de l'événement: ".$ligne['referrer']."<br>"." Date de création: ".$ligne['createdAt']."<br>"."<br>";
+}
+});
+
 
 $app->run();
 
