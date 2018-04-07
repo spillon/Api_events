2a80eef5658bdc5e7c75f7630d5925d085cf2f01
Pillon <pillonsteve@gmail.com>
2018-04-07 12:41:36 +0200

    correction des erreurs sur le dépot

--- index.php
+++ index.php
@@ -42,19 +42,21 @@ $tmp = $args['id'];
 $req = $conn->query("SELECT * FROM events WHERE cookie_id = $tmp;");
 foreach($req as $ligne)
 {
-	echo "Nom de l'événement: ".$ligne['name']."<br>"." Référence de l'événement: ".$ligne['referrer']."<br>"." Date de création: ".$ligne['createdAt'];
+	echo "Nom de l'événement n° $tmp: ".$ligne['name']."<br>"." Référence de l'événement: ".$ligne['referrer']."<br>"." Date de création: ".$ligne['createdAt'];
 }
 });
 
 $app->get("/api/dashboard",function(){
 $conn = connect_to_db();
-$req = $conn->query("SELECT * FROM events");
+setlocale(LC_TIME, 'fra_fra');
+date_default_timezone_set("Europe/Brussels");
+echo strftime('Date: %A %d %B %Y, Heure: %H:%M').'<br>'.'<br>';
+$req = $conn->query("SELECT * FROM events ORDER BY createdAt");
 foreach($req as $ligne)
 {
 	echo "Nom de l'événement: ".$ligne['name']."<br>"." Référence de l'événement: ".$ligne['referrer']."<br>"." Date de création: ".$ligne['createdAt']."<br>"."<br>";
 }
 });
-
 
 $app->run();
 
