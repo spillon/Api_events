ea6fba8b5e8294e9aed65e078e05360f407b143c
Pillon <pillonsteve@gmail.com>
2018-04-09 13:11:23 +0200

    maj du json

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
- 
+
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Reponse;
+
 require "vendor/autoload.php";
+
 function connect_to_db()
 {
 	$servername = "localhost";
@@ -23,10 +27,8 @@ $app = new Slim\App();
 $app->get("/api/events",function(){
 $conn = connect_to_db();
 $req = $conn->query("SELECT * FROM events");
-foreach($req as $ligne)
-{
-	echo json_encode($ligne['name']);
-}
+$res = $req->fetchALL(PDO::FETCH_OBJ);
+return json_encode($res);
 });
 
 $app->post("/api/events",function($request){
@@ -40,19 +42,15 @@ $app->get('/api/events/{id}',function($request, $response, $args){
 $conn = connect_to_db();
 $tmp = $args['id'];
 $req = $conn->query("SELECT * FROM events WHERE cookie_id = $tmp;");
-foreach($req as $ligne)
-{
-	echo json_encode($ligne['name'].$ligne['referrer'].$ligne['createdAt']);
-}
+$res = $req->fetchALL(PDO::FETCH_OBJ);
+return json_encode($res);
 });
 
 $app->get("/api/dashboard",function(){
 $conn = connect_to_db();
 $req = $conn->query("SELECT * FROM events ORDER BY createdAt");
-foreach($req as $ligne)
-{
-	echo json_encode($ligne['name'].$ligne['referrer'].$ligne['createdAt']);
-}
+$res = $req->fetchALL(PDO::FETCH_OBJ);
+return json_encode($res);
 });
 
 $app->run();
